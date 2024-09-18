9d822170e5a8209dd963b7169e6ea1e64f2c7548
Denis Istomin <rex.hd.kanal@gmail.com>
2024-09-19 01:08:12 +0300

    add foreign keys to tables, split rooms table to room_info and group_rooms tables, add room types

--- init.sql
+++ init.sql
@@ -1,31 +1,49 @@
 CREATE DATABASE IF NOT EXISTS chatdb;
 use chatdb;
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS user_info (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    login VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    tag VARCHAR(255) NOT NULL,
+        UNIQUE(tag),
     name VARCHAR(255)
-        CHARACTER SET utf8,
-    UNIQUE(login)
-
+        CHARACTER SET utf8
+);
+CREATE TABLE IF NOT EXISTS room_info(
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+    type ENUM('direct','group')
+);
+CREATE TABLE IF NOT EXISTS user_credentials(
+    login VARCHAR(255) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    userId INT,
+    FOREIGN KEY (userId) REFERENCES user_info (id)
+         ON DELETE CASCADE
+ 
 );
 CREATE TABLE IF NOT EXISTS room_users (
-    roomId INT NOT NULL,
-    userId INT NOT NULL
+    userId INT,
+    roomId INT,
+    FOREIGN KEY (roomId) REFERENCES room_info (id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (userId) REFERENCES user_info (id)
+        ON DELETE CASCADE
 );
-CREATE TABLE IF NOT EXISTS rooms (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    tag VARCHAR(255)NOT NULL,
+CREATE TABLE IF NOT EXISTS group_rooms (
+    tag VARCHAR(255) PRIMARY KEY,
+    UNIQUE(tag),
     name VARCHAR(255)
         CHARACTER SET utf8,
-    UNIQUE(tag)
-
+    roomId INT,
+    FOREIGN KEY (roomId) REFERENCES room_info (id)
 );
 CREATE TABLE IF NOT EXISTS room_messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    roomId INT NOT NULL,
-    userId INT NOT NULL,
     time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     body VARCHAR(2048)
-        CHARACTER SET utf8
+        CHARACTER SET utf8,
+    roomId INT,
+    FOREIGN KEY (roomId) REFERENCES room_info (id)
+        ON DELETE CASCADE,
+    userId INT,
+    FOREIGN KEY (userId) REFERENCES user_info (id)
+        ON DELETE SET NULL
 );
